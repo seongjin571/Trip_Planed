61dde3209f8e08a4e1d16edfd205bd39a736aaac
seongjin571 <gkstjdwls11@naver.com>
2019-07-13 20:23:39 +0900

    충돌 충돌

--- js/plan.js
+++ js/plan.js
@@ -144,17 +144,23 @@ storePlanBtn.addEventListener('click', function () {
 
     const planCount = document.querySelectorAll('.one-plan');
     let data = [];
-
+    const planTitle = document.querySelector('.plan-title');
     for (let i = 0; i < planCount.length; i++) {
+        let typeId = document.getElementsByClassName('one-plan')[i].dataset.typeId
         data[i] = {
             start_time: document.getElementsByClassName('start-time')[i].value,
             finish_time: document.getElementsByClassName('finish-time')[i].value,
             plan_content: document.getElementsByClassName('plan-content')[i].value,
             day: document.getElementsByClassName('one-plan')[i].parentElement.parentElement.dataset.day,
-            plan_title: document.querySelector('.plan-title').innerHTML,
-            tour_flag: 0,
-            member_id: 'seongjin571',
-            total_day: document.querySelector('.plan-title').dataset.totalday
+            plan_title: planTitle.innerHTML,
+            tour_flag: typeId,
+            member_id: planTitle.dataset.id,
+            total_day: planTitle.dataset.totalDay,
+            start_day: planTitle.dataset.startDay,
+            finsish_day: planTitle.dataset.finishDay,
+            big_city: document.querySelector('.big-city').innerHTML,
+            small_city: document.querySelector('.small-city').innerHTML,
+            
         }
     }
     addPlanHttp.open("POST", "/addPlan");
